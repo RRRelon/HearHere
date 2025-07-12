4886b758e31c8bd596360719191c1d414f7d4aa8
jungse8609 <jungse8609@naver.com>
2025-07-12 09:14:52 +0900

    feat(#4): Scene 전환 기능 구현
    - SceneLoader: 새로운 씬 로드와 기존 씬 언로드를 담당
    - InitializationLoader: 게임 처음 시작 시 MainMenu와 PersistenceManagers 씬 로드
    - SceneController: 전환하고 싶은 씬을 설정해 이동 담당
    - EditorColdStartup: Initialization 씬을 거치지 않고 바로 원하는 씬에서 동작하기 위한 도우미 역할
    
    - LoadEventChannelSO: Observer pattern 구현을 위한 이벤트 채널. Listener와 Broadcaster가 존재

---
+++ HearHere/Assets/Scripts/SceneManagement/SceneController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class SceneController : MonoBehaviour
+{
+    [SerializeField] private AssetReference sceneToLoad;
+    
+    [Header("Broadcasting on")]
+    [SerializeField] private LoadEventChannelSO sceneLoadChannel;
+
+    /// <summary>
+    /// 버튼 액션 바인딩
+    /// </summary>
+    public void SceneLoad()
+    {
+        sceneLoadChannel.OnLoadingRequested(sceneToLoad);
+    }
+}
